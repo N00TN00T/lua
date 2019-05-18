9162e0378079436e326c4ca9f2cb2fe9c9ffcbfb
Charlie <charlie.challe@live.se>
2019-05-18 15:11:13 +0200

    Set up premake file and .gitignore for Wizzy

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+-- PREMAKE FOR LUA
+
+project "lua"
+    kind "StaticLib"
+    language "C"
+
+	targetdir ("bin/" .. output_dir .. "/%{prj.name}")
+    objdir ("bin-int/" .. output_dir .. "/%{prj.name}")
+
+	files
+    {
+        "**.h",
+        "**.c"
+    }
+
+    includedirs { "" }
+
+	filter "system:windows"
+        systemversion "latest"
+        staticruntime "on"
+
+    filter "system:linux"
+        systemversion "latest"
+        staticruntime "on"
+        pic "on"
+
+    filter "system:macosx"
+        systemversion "latest"
+        staticruntime "on"
+
+    filter "configurations:Debug*"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release*"
+        runtime "Release"
+        symbols "on"
+        optimize "on"
+
+    filter "configurations:Dist*"
+        runtime "Release"
+        optimize "on"
